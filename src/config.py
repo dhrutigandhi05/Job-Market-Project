2af4c92970e69127b3dc1d6ca3c7cb868cb99cff
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-07-17 18:01:15 -0400

    connect to s3 client

--- src/config.py
+++ src/config.py
@@ -11,3 +11,11 @@ AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+
+def get_s3_client():
+    return boto3.client(
+        's3',
+        region_name=AWS_DEFAULT_REGION,
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    )
