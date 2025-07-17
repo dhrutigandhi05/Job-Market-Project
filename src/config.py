ece5613a16481a8d698a03ca7a87b36593ee0868
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-07-17 18:05:36 -0400

    connection to postgres placeholder

--- src/config.py
+++ src/config.py
@@ -19,3 +19,11 @@ def get_s3_client():
         aws_access_key_id=AWS_ACCESS_KEY_ID,
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
+
+# db connection placeholder
+DATABASE_URL = os.getenv("DATABASE_URL")
+def get_db_engine():
+    if DATABASE_URL:
+        return create_engine(DATABASE_URL)
+    else:
+        raise ValueError("DATABASE_URL is not set in the environment variables.")
