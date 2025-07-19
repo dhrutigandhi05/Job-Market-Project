97c7fc52d680cadd669ce185ee06f771e53ccb2a
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-07-19 14:37:08 -0400

    try to fix ingestion error from lambda function

--- src/config.py
+++ src/config.py
@@ -9,8 +9,8 @@ load_dotenv()
 RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
 RAPIDAPI_HOST = os.getenv("RAPIDAPI_HOST")
 AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+# AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -18,9 +18,9 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 def get_s3_client():
     return boto3.client(
         's3',
-        region_name=AWS_DEFAULT_REGION,
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+        region_name=AWS_DEFAULT_REGION
+        # aws_access_key_id=AWS_ACCESS_KEY_ID,
+        # aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
 
 # postgres db connection
