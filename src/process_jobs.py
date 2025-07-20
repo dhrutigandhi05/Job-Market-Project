7991b376622ca1998905dc5649950380ce51caa1
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-07-20 12:17:07 -0400

    list all s3 files functions

--- src/process_jobs.py
+++ src/process_jobs.py
@@ -4,3 +4,9 @@ import pandas as pd
 from datetime import datetime
 from config import get_s3_client, get_db_engine, S3_BUCKET_NAME
 
+# list all files in the specified S3 bucket with the given prefix
+def list_s3_files(prefix: str) -> list[str]:
+    s3 = get_s3_client()
+    response = s3.list_objects_v2(Bucket=S3_BUCKET_NAME, Prefix=prefix)
+    return [o['Key'] for o in response.get('Contents', [])]
+
