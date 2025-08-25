b799bd03304b9b0fa3c30aad0e49216bd8ac0c91
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-08-25 19:46:26 -0400

    create sqlalchemy engine

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -4,3 +4,9 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 from sqlalchemy import create_engine, text
+
+def get_engine():
+    cfg = st.secrets["db"]
+    url = (f"postgresql+psycopg2://{cfg['user']}:{cfg['password']}"
+           f"{cfg['host']}:{cfg['port']}//{cfg['database']}")
+    return create_engine(url, pool_pre_ping=True)
